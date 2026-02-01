eeaa9ee7303a9bfa1e937c9f5a9e47ca2a480b9f
damtuyetnhi06022009 <damtuyetnhi06022009@gmail.com>
2026-02-01 16:40:55 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Welcome from './welcome'
+import Student from './student'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div className='card-title'>
-        <Welcome name =" Tuyết Nhi "/>
+        <Student name =" Tuyết Nhi "/>
         <p>Tên: Đàm Tuyết Nhi </p>
         <p>Lớp: 26SPIT3 </p>
         <p>Ngày sinh: 6/2/2009 </p>
